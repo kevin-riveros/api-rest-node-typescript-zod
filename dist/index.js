d56bc63b31315bdc2feac0b27eaa302df1298190
kevin-riveros <riverosk885@gmail.com>
2023-07-31 00:11:01 -0500

    update gitignore

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+let __importDefault = (this && this.__importDefault) || function (mod) {
+  return (mod && mod.__esModule) ? mod : { default: mod }
+};
+Object.defineProperty(exports, '__esModule', { value: true })
+const express_1 = __importDefault(require('express'))
+const app = (0, express_1.default)()
+app.use(express_1.default.json())
+app.get('/', (req, res) => {
+  res.send('API EXPRESS TS')
+})
+app.listen(3000)
+console.log('server on port', 3000)
